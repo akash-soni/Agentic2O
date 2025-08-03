9a84ae22a0b39e0a1b02c813748c8dfcadc2adff
akash.soni <akash.200287@gmail.com>
2025-08-04 00:14:50 +0530

    changes

---
+++ 03-Autogen/07-Autogen Projects/02-AnalyserGPT-modular/main.py
@@ -0,0 +1,31 @@
+import asyncio
+from teams.analyzer_gpt import getDataAnalyzerTeam
+from models.openai_model_client import get_model_client
+from config.docker_utils import getDockerCommandLineExecutor, start_docker_container, stop_docker_container
+from autogen_agentchat.messages import TextMessage
+
+
+
+async def main():
+
+    openai_model_client = get_model_client()
+    docker = getDockerCommandLineExecutor()
+
+    team = getDataAnalyzerTeam(docker,openai_model_client)
+
+    try:
+        task = 'Can you give me a graph of survived and died in my data titanic.csv'
+
+        await start_docker_container(docker)
+
+        async for message in team.run_stream(task=task):
+            print(message)
+
+    except Exception as e:
+        print(e)
+    finally:
+        await stop_docker_container(docker)
+
+
+if(__name__=='__main__'):
+    asyncio.run(main())
---
+++ 03-Autogen/10-Autogen-MCP/mcp_in_autogen.py
@@ -0,0 +1,46 @@
+from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+import asyncio 
+import time
+from autogen_ext.tools.mcp import McpWorkbench, StdioServerParams
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+
+async def main():
+
+    params = StdioServerParams(
+    command = 'uvx',
+    args=['mcp-server-time','--local-timezone=America/New_York']
+    )
+
+    model = OpenAIChatCompletionClient(
+        model='gpt-4o',
+        api_key=os.getenv('OPENAI_API_KEY')
+    )
+
+    async with McpWorkbench(server_params=params) as workbench:
+
+        # tools = await workbench.list_tools()
+        # print(tools)
+        agent = AssistantAgent(
+            name='Agent',
+            system_message='You are a helpful assistant',
+            model_client=model,
+            workbench=workbench,
+            reflect_on_tool_use=True
+        )
+
+
+        task = 'What is the time right now in Bengaluru ?'
+
+        async for message in agent.run_stream(task=task):
+            print("-"*100)
+            print(message)
+            print('-'*100)
+
+if(__name__=='__main__'):
+    asyncio.run(main())
+
